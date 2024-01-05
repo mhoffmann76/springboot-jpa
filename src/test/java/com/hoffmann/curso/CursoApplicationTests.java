68221f57c515af81b23cff23caab00119413c50f
mhoffmann76 <hoffmannmarcelo@hotmail.com>
2024-01-05 13:53:57 -0300

    Created Project

---
+++ src/test/java/com/hoffmann/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoffmann.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
