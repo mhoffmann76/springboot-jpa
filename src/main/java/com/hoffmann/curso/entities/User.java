2f0767f73a9612ca94ebe0950385755a04abe3f7
mhoffmann76 <hoffmannmarcelo@hotmail.com>
2024-01-05 17:19:28 -0300

    H2 database, test profile, JPA

--- src/main/java/com/hoffmann/curso/entities/User.java
+++ src/main/java/com/hoffmann/curso/entities/User.java
@@ -1,5 +1,6 @@
 package com.hoffmann.curso.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,11 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
